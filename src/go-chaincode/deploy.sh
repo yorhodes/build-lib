0e63ceea79292e9a17443a801491efa8ea12293e
Yorke Rhodes IV <Yorke.Rhodes.IV@ibm.com>
2018-07-19 17:40:51 -0400

    Fix configpath environment variable

--- src/go-chaincode/deploy.sh
+++ src/go-chaincode/deploy.sh
@@ -6,12 +6,12 @@ source "${SCRIPT_DIR}/common/env.sh"
 source "${SCRIPT_DIR}/common/utils.sh"
 source "${SCRIPT_DIR}/common/blockchain.sh"
 
-if [[ ! -f $CONFIG_PATH ]]; then
-  echo "No deploy configuration at specified path: ${CONFIG_PATH}"
+if [[ ! -f $CONFIGPATH ]]; then
+  echo "No deploy configuration at specified path: ${CONFIGPATH}"
   exit 1
 fi
 
 install_jq
 setup_service_constants
 provision_blockchain
-parse_fabric_config $CONFIG_PATH
+parse_fabric_config $CONFIGPATH
