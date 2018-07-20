8e0e90313939321ea74fc341c6951aef019faa91
jorgedr94 <jorgedr@gmail.com>
2018-07-19 20:06:06 -0400

    Update utils.sh

--- src/common/utils.sh
+++ src/common/utils.sh
@@ -3,12 +3,14 @@
 # Common utility functions, e.g. to make curl requests
 
 function install_jq {
+    set -x
     curl -o jq -L https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
     chmod +x jq
     export PATH=${PATH}:${PWD}
 }
 
 function do_curl {
+    set -x
     HTTP_RESPONSE=$(mktemp)
     HTTP_STATUS=$(curl -w '%{http_code}' -o ${HTTP_RESPONSE} "$@")
     cat ${HTTP_RESPONSE}
