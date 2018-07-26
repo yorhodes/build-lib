d6b9c7ca62a673f6826a3dc17660faa802a053bd
Yorke Rhodes IV <Yorke.Rhodes.IV@ibm.com>
2018-07-26 11:04:14 -0400

    Add XML report to test stage
    Signed-off-by: Yorke Rhodes IV <Yorke.Rhodes.IV@ibm.com>

--- src/go-chaincode/test.sh
+++ src/go-chaincode/test.sh
@@ -7,4 +7,6 @@ source "${SCRIPT_DIR}/common/env.sh"
 
 $DEBUG && set -x
 
-go test -v chaincode
+go get -u github.com/jstemmer/go-junit-report
+
+go test -v chaincode 2>&1 | go-junit-report > report.xml
